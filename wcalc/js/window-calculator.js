1680cf0de578efa34ed9e72f0ef05addc42be851
Nikolai Krivetski <nikolaikrivetski@coherentsolutions.com>
2015-10-07 01:13:30 +0300

    Added price section'

--- wcalc/js/window-calculator.js
+++ wcalc/js/window-calculator.js
@@ -1,14 +1,29 @@
-$(function() {
-   
-    
-    $('.wc-slider-vertical').slider( {
+$(function () {
+
+
+    $('.wc-slider-vertical').slider({
         orientation: "vertical",
         range: "min",
         min: 0,
-        max: 100,
-        value: 60,
-        slide: function(event, ui) {
-            
+        max: 4000,
+        value: 1500,
+        slide: function (event, ui) {
+            $(".wc-vertical-input").val(ui.value);
         }
     });
+
+    $(".wc-vertical-input").val($('.wc-slider-vertical').slider("value"));
+
+    $('.wc-slider-horizontal').slider({
+        orientation: "horizontal",
+        range: "min",
+        min: 0,
+        max: 3000,
+        value: 1700,
+        slide: function (event, ui) {
+            $(".wc-horizontal-input").val(ui.value);
+        }
+    });
+
+    $(".wc-horizontal-input").val($('.wc-slider-horizontal').slider("value"));
 });
